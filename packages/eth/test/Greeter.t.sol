acc6d31ec40aefb5db5bf55691899abf19379c75
Scott Moses Sunarto <github@smsunarto.com>
2022-08-07 04:29:56 -0700

    Initial commit

---
+++ packages/eth/test/Greeter.t.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.15;
+
+import "forge-std/Test.sol";
+
+import {Greeter} from "src/Greeter.sol";
+
+contract GreeterTest is Test {
+    using stdStorage for StdStorage;
+
+    Greeter greeter;
+
+    event GMEverybodyGM();
+
+    function setUp() public {
+        greeter = new Greeter("gm");
+    }
+
+    // VM Cheatcodes can be found in ./lib/forge-std/src/Vm.sol
+    // Or at https://github.com/foundry-rs/forge-std
+    function testSetGm() public {
+        greeter.setGreeting("gm gm");
+
+        // Expect the GMEverybodyGM event to be fired
+        vm.expectEmit(true, true, true, true);
+        emit GMEverybodyGM();
+        greeter.gm("gm gm");
+
+        // Expect the gm() call to revert
+        vm.expectRevert(abi.encodeWithSignature("BadGm()"));
+        greeter.gm("gm");
+
+        // We can read slots directly
+        uint256 slot = stdstore.target(address(greeter)).sig(greeter.owner.selector).find();
+        assertEq(slot, 1);
+        bytes32 owner = vm.load(address(greeter), bytes32(slot));
+        assertEq(address(this), address(uint160(uint256(owner))));
+    }
+}
