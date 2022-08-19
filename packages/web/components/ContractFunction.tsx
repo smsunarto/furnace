13a6a03cc394b10ee9848e7bf3832acc57b571b7
Sehyun Chung <sehyun@berkeley.edu>
2022-08-18 22:44:44 -0400

    refactor

--- packages/web/components/ContractFunction.tsx
+++ packages/web/components/ContractFunction.tsx
@@ -1,15 +1,16 @@
+import { Box, Button, Flex, Heading, Input, Text } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import {
   useContractRead,
-  usePrepareContractWrite,
   useContractWrite,
+  usePrepareContractWrite,
 } from 'wagmi'
 import GreeterAbi from '../abi/Greeter.sol/Greeter.json'
 const abi = GreeterAbi.abi
 
 type Props = {
   inputs: Input[]
-  name?: string
+  name: string
   stateMutatability: string
   type: string
 }
@@ -26,12 +27,12 @@ export const ContractFunction: React.FC<Props> = ({
   stateMutatability,
   type,
 }) => {
-  if (type !== 'function') return
+  if (type !== 'function') return null
   const Content = stateMutatability === 'view' ? ContractRead : ContractWrite
   return (
     <div
       style={{
-        borderBottom: '1px solid gray',
+        // borderBottom: '1px solid gray',
         padding: '2rem 0',
       }}
     >
@@ -52,7 +53,7 @@ const ContractRead: React.FC<Props> = ({
   type,
 }) => {
   const { refetch, data, isLoading, isError } = useContractRead({
-    addressOrName: '0x5fbdb2315678afecb367f032d93f642f64180aa3',
+    addressOrName: '0xB41DB45C57347669704C6E05305273A6b21e4b71',
     contractInterface: abi,
     functionName: name,
     enabled: false,
@@ -66,18 +67,15 @@ const ContractRead: React.FC<Props> = ({
   }
 
   return (
-    <>
-      <h1>
-        <b>{name}</b>
-      </h1>
-      <button
-        onClick={() => handleRead()}
-        style={{ border: '1px solid #eaeaea', padding: '0.5rem 1rem' }}
-      >
+    <Box backgroundColor="whiteAlpha.200" borderRadius="12px" padding="24px">
+      <Heading fontSize="lg" fontFamily="IBM Plex Mono">
+        {name}
+      </Heading>
+      <Button my="6px" onClick={handleRead}>
         call
-      </button>
-      {value && <p>Output: {value}</p>}
-    </>
+      </Button>
+      {value && <Text>Output: {value}</Text>}
+    </Box>
   )
 }
 
@@ -90,7 +88,7 @@ const ContractWrite: React.FC<Props> = ({
   const [value, setValue] = useState('')
 
   const { config } = usePrepareContractWrite({
-    addressOrName: '0x5fbdb2315678afecb367f032d93f642f64180aa3',
+    addressOrName: '0xB41DB45C57347669704C6E05305273A6b21e4b71',
     contractInterface: abi,
     functionName: name,
     args: [value],
@@ -98,49 +96,43 @@ const ContractWrite: React.FC<Props> = ({
 
   const { data, isLoading, isSuccess, write } = useContractWrite(config)
 
-  const handleChange = (event) => {
-    setValue(event.target.value)
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value)
   }
 
-  console.log(inputs)
+  // console.log(inputs)
 
   return (
-    <>
-      <h1>
-        <b>{name}</b>
-      </h1>
+    <Box backgroundColor="whiteAlpha.200" borderRadius="12px" padding="24px">
+      <Heading fontSize="lg" fontFamily="IBM Plex Mono">
+        {name}
+      </Heading>
       {inputs &&
         inputs.map((input) => {
           return (
-            <div key={input.name} style={{ display: 'flex', gap: '12px' }}>
-              <p>{input.name}:</p>
-              <div className="mb-3 pt-0">
-                <input
-                  type="text"
-                  className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-                  name={input.name}
-                  onChange={handleChange}
-                  placeholder={input.type}
-                />
-              </div>
-            </div>
+            <Flex
+              alignItems="center"
+              key={input.name}
+              style={{ display: 'flex', gap: '12px' }}
+            >
+              <Text>{input.name}:</Text>
+              <Input
+                name={input.name}
+                onChange={handleChange}
+                placeholder={input.type}
+                maxWidth="144px"
+                type="text"
+                variant="filled"
+              />
+            </Flex>
           )
         })}
 
-      <button
-        disabled={!write}
-        onClick={() => write?.()}
-        style={{
-          border: '1px solid #eaeaea',
-          cursor: !write ? 'not-allowed' : 'pointer',
-          padding: '0.5rem 1rem',
-          opacity: !write ? 0.5 : 1,
-        }}
-      >
+      <Button disabled={!write} onClick={() => write?.()}>
         call
-      </button>
+      </Button>
       {isLoading && <div>Check Wallet</div>}
       {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
-    </>
+    </Box>
   )
 }
